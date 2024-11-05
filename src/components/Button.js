95e32e568c17460d3ccc71534f56123eb67beb53
Saba <sabanes048@gmail.com>
2024-11-05 17:19:34 +0000

    layout done

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+
+const Button = ({ text }) => {
+  return (
+    <div className="w-[90%] font-bold max-w-[500px] border-solid border-2 border-[#061592] hover:bg-[#061592] hover:text-[#E0E0E0] transition-colors duration-300 rounded-xl py-4 text-lg">
+            <h1 className="text-center">{text}</h1>
+          </div>
+  )
+}
+
+export default Button
