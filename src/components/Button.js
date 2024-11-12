dbb9b508e2a0bc6ccdaf6ce15fd2a20988a6a0a1
Saba <sabanes048@gmail.com>
2024-11-12 13:30:19 +0000

    links added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,11 @@
-
-const Button = ({ text }) => {
+const Button = ({ text, link }) => {
   return (
-    <div className="w-[90%] font-bold max-w-[500px] border-solid border-2 border-[#061592] hover:bg-[#061592] hover:text-[#E0E0E0] transition-colors duration-300 rounded-xl py-4 text-lg">
-            <h1 className="text-center">{text}</h1>
-          </div>
-  )
-}
+    <a className="w-screen" href={link} target="blank">
+      <div className="px-[80px] w-[80%] max-w-[500px] m-auto font-bold border-solid border-2 border-[#061592] hover:bg-[#061592] hover:text-[#E0E0E0] transition-colors duration-300 rounded-xl py-4 text-lg">
+        <h1 className="text-center">{text}</h1>
+      </div>
+    </a>
+  );
+};
 
-export default Button
+export default Button;
